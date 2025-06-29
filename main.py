72bcdf5e8bde6ce6b4cdbffe2322e5e5a193de0f
jonijasper <jasper.alakotila@protonmail.com>
2025-06-29 22:51:33 +0300

    small changes (prepping cleanup() usage as a module)

--- main.py
+++ main.py
@@ -14,26 +14,9 @@ main.py -f "./inputfile.html" -o "./outputfile.html"
     as "new_inputfilename.html"
 """
 
-import sys
 from parsers.html_cleaner import HTMLCleaner
 from parsers.taglib import TagLib
 
-
-def argparser(*args) -> list:
-    argdict = {"-f": None, "-o": None}
-
-    if "-h" in args or "--help" in args:
-        print(__doc__)
-        exit()
-    elif "--demo" in args:
-        _demo()
-        exit()
-    else:
-        for i, arg in enumerate(args):
-            if arg in argdict:
-                argdict[arg] = args[i+1]
-
-    return list(argdict.values())
 
 def cleanup(html_doc: str, newname: str = ""):
     with open(html_doc, 'r') as f:
@@ -51,6 +34,22 @@ def cleanup(html_doc: str, newname: str = ""):
     cleaner.feed(html_str)
     cleaner.close()
 
+def _argparser(*args) -> list:
+    argdict = {"-f": None, "-o": None}
+
+    if "-h" in args or "--help" in args:
+        print(__doc__)
+        exit()
+    elif "--demo" in args:
+        _demo()
+        exit()
+    else:
+        for i, arg in enumerate(args):
+            if arg in argdict:
+                argdict[arg] = args[i+1]
+
+    return list(argdict.values())
+
 def _demo():
     demoA = ["./demo/sourceA.html", "./demo/demoA.html"]
     demoB = ["./demo/sourceB.html", "./demo/demoB.html"]
@@ -59,7 +58,9 @@ def _demo():
 
 
 if __name__ == "__main__":
-    args = argparser(*sys.argv[1:])
+    import sys
+
+    args = _argparser(*sys.argv[1:])
     if args[0]:
         cleanup(*args)
     else:
