ba5d677129d7248aa47d0e4734974464d98b15ff
jonijasper <jasper.alakotila@protonmail.com>
2025-06-29 17:22:17 +0300

    taglib for searching void tags before parsing

---
+++ taglib.py
@@ -0,0 +1,47 @@
+from html.parser import HTMLParser
+
+class TagLib(HTMLParser):
+    def __init__(self):
+        HTMLParser.__init__(self)
+
+        self.taglist = []
+        self.voidtags = []
+        self.weird = []
+
+    def handle_starttag(self, tag, attrs):
+        self.taglist.append(tag)
+
+    def handle_endtag(self, tag):
+        if tag in self.taglist:
+            while self.taglist[-1] != tag:
+                self.voidtags.append(self.taglist.pop())
+            self.taglist.pop()
+
+        else:
+            pos = self.getpos()[0]
+            self.weird.append((tag,pos))
+
+    def handle_startendtag(self, tag, attrs):
+        self.voidtags.append(tag)
+
+    def get_voidtags(self):
+        if self.weird:
+            print(f"Found end tags without start tag:")
+            for tag,pos in self.weird:
+                print(f"</{tag}>, line {pos}")
+
+        return set(self.voidtags)
+
+
+if __name__ == "__main__":
+    testdoc = "./tests/html_test_doc.html"
+    
+    with open(testdoc,'r') as f:
+        html_doc = f.read()
+
+    foo = TagLib()
+    foo.feed(html_doc)
+    print(f"{foo.get_voidtags()=}")
+    foo.close()
+    
+    print('*** \N{goat}')
